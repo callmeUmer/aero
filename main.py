089d7da19db5b55693768d2b4d83e33a5499b0d2
Umar Sohail <umarsohail43742@hotmail.com>
2018-11-14 01:00:34 -0800

    formatted in PEP8

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ DESCRIPTION: Minimalistic, Customizable Code Editor
 """
 
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import (QApplication, QWidget, QInputDialog,
+                             QLineEdit, QFileDialog, QMainWindow, QMessageBox)
 from design import Ui_MainWindow
 
 
@@ -18,44 +19,56 @@ class mainWindow(QMainWindow, Ui_MainWindow):
         self.FILENAME = None
         self.initUI()
 
-    #initialization of UI
+    # initialization of UI
     def initUI(self):
         self.WindowTitle = "Python Code Editor"
         self.actionOpen.triggered.connect(self.openFileNameDialog)
         self.actionSave.triggered.connect(self.saveFile)
         self.actionSave_As.triggered.connect(self.saveAsFileNameDialog)
 
+    # function for opening a file
 
-    #function for opening a file
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        filename, _= QFileDialog.getOpenFileName(self,"Open Files", "","All Files (*);;Python Files (*.py);; C++ Files (*.cpp)", options=options)
+        filename, _ = QFileDialog.getOpenFileName(
+            self, "Open Files",
+            "",
+            "All Files (*);;Python Files (*.py);; C++ Files (*.cpp)",
+            options=options)
         if filename:
             self.FILENAME = filename
             with open(filename, 'r') as file:
                 read_file = file.read()
                 self.textEdit.setPlainText(read_file)
 
+    # function for saving changes to the opened file
 
-    #function for saving changes to the opened file
     def saveFile(self):
         if self.FILENAME:
             with open(self.FILENAME, 'w') as save:
                 text = self.textEdit.toPlainText()
                 save.write(text)
         else:
-            QMessageBox.warning(self, 'No file opened', "No file opened", QMessageBox.Yes | QMessageBox.No)
-
+            QMessageBox.warning(
+                self,
+                'No file opened',
+                "No file opened",
+                QMessageBox.Yes | QMessageBox.No)
 
     # function for saving changes to a new file
+
     def saveAsFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        filename, _= QFileDialog.getSaveFileName(self,"Save File","","All Files (*);; Python Files (*.py);; C++ Files(*.cpp)", options=options)
+        filename, _ = QFileDialog.getSaveFileName(
+            self, "Save File",
+            "",
+            "All Files (*);; Python Files (*.py);; C++ Files(*.cpp)",
+            options=options)
         split_ex = _.split(' ')
         extension = ''
-        
+
         if filename:
             if split_ex[0] == 'C++':
                 extension += '.cpp'
@@ -63,14 +76,13 @@ class mainWindow(QMainWindow, Ui_MainWindow):
                 extension += '.py'
             else:
                 pass
-                
-            
-            with open(filename+extension, 'w+') as saveAs:
+
+            with open(filename + extension, 'w+') as saveAs:
                 self.FILENAME = saveAs
                 text = self.textEdit.toPlainText()
                 saveAs.write(text)
                 saveAs.close()
-            
+
 
 if __name__ == '__main__':
     import sys
