e9d93faa63111363203677e6ead33e3f0eb8f17e
Umar Sohail <umarsohail43742@hotmail.com>
2018-11-14 00:46:45 -0800

    pulled repo from github to add readme file

---
+++ design.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(938, 709)
+        MainWindow.setMinimumSize(QtCore.QSize(22, 0))
+        MainWindow.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit_2.setMinimumSize(QtCore.QSize(40, 0))
+        self.textEdit_2.setMaximumSize(QtCore.QSize(40, 16777215))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.gridLayout.addWidget(self.textEdit_2, 1, 0, 1, 1)
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setObjectName("textEdit")
+        self.gridLayout.addWidget(self.textEdit, 1, 1, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 938, 21))
+        self.menubar.setObjectName("menubar")
+        self.menu_Files = QtWidgets.QMenu(self.menubar)
+        self.menu_Files.setObjectName("menu_Files")
+        self.menuEdit = QtWidgets.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionOpen = QtWidgets.QAction(MainWindow)
+        self.actionOpen.setObjectName("actionOpen")
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        self.actionSave.setObjectName("actionSave")
+        self.actionSave_As = QtWidgets.QAction(MainWindow)
+        self.actionSave_As.setObjectName("actionSave_As")
+        self.menu_Files.addAction(self.actionOpen)
+        self.menu_Files.addAction(self.actionSave)
+        self.menu_Files.addAction(self.actionSave_As)
+        self.menubar.addAction(self.menu_Files.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menu_Files.setTitle(_translate("MainWindow", "&Files"))
+        self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
+        self.actionOpen.setText(_translate("MainWindow", "Open"))
+        self.actionSave.setText(_translate("MainWindow", "Save"))
+        self.actionSave_As.setText(_translate("MainWindow", "Save As"))
---
+++ main.py
@@ -0,0 +1,80 @@
+"""
+-*- coding: utf-8 -*-
+Author : UMAR SOHAIL
+DESCRIPTION: Minimalistic, Customizable Code Editor
+"""
+
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QMainWindow, QMessageBox
+from design import Ui_MainWindow
+
+
+class mainWindow(QMainWindow, Ui_MainWindow):
+    """ mainWindow class to display mainwindows and widgets """
+
+    def __init__(self, parent=None):
+        super(mainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.FILENAME = None
+        self.initUI()
+
+    #initialization of UI
+    def initUI(self):
+        self.WindowTitle = "Python Code Editor"
+        self.actionOpen.triggered.connect(self.openFileNameDialog)
+        self.actionSave.triggered.connect(self.saveFile)
+        self.actionSave_As.triggered.connect(self.saveAsFileNameDialog)
+
+
+    #function for opening a file
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        filename, _= QFileDialog.getOpenFileName(self,"Open Files", "","All Files (*);;Python Files (*.py);; C++ Files (*.cpp)", options=options)
+        if filename:
+            self.FILENAME = filename
+            with open(filename, 'r') as file:
+                read_file = file.read()
+                self.textEdit.setPlainText(read_file)
+
+
+    #function for saving changes to the opened file
+    def saveFile(self):
+        if self.FILENAME:
+            with open(self.FILENAME, 'w') as save:
+                text = self.textEdit.toPlainText()
+                save.write(text)
+        else:
+            QMessageBox.warning(self, 'No file opened', "No file opened", QMessageBox.Yes | QMessageBox.No)
+
+
+    # function for saving changes to a new file
+    def saveAsFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        filename, _= QFileDialog.getSaveFileName(self,"Save File","","All Files (*);; Python Files (*.py);; C++ Files(*.cpp)", options=options)
+        split_ex = _.split(' ')
+        extension = ''
+        
+        if filename:
+            if split_ex[0] == 'C++':
+                extension += '.cpp'
+            elif split_ex[0] == 'Python':
+                extension += '.py'
+            else:
+                pass
+                
+            
+            with open(filename+extension, 'w+') as saveAs:
+                self.FILENAME = saveAs
+                text = self.textEdit.toPlainText()
+                saveAs.write(text)
+                saveAs.close()
+            
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    sim = mainWindow()
+    sim.show()
+    sys.exit(app.exec_())
