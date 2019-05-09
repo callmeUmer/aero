158e1f2042b8a7257c23fe983bfaafa61535c2d3
Umar Sohail <umarsohail43742@gmail.com>
2019-05-09 11:37:08 -0700

    maked full number line but it is not syncing with main textEditor widget

--- src/main.py
+++ src/main.py
@@ -5,7 +5,15 @@ import platform
 import os
 
 
-class mainWindow:
+def count_monkeypatch(self, index1, index2, *args):
+    args = [self._w, "count"] + ["-" + arg for arg in args] + [index1, index2]
+    result = self.tk.call(*args)
+    return result
+
+Text.count = count_monkeypatch
+
+
+class mainWindow(Tk):
 
     def __init__(self, root):
         self.root = root
@@ -25,17 +33,21 @@ class mainWindow:
         self.mainMenu.add_cascade(label='Edit', menu=self.fileMenu,)
 
         #number line
+        self.scroll = Scrollbar(self.root, orient=VERTICAL, command=self._simulScroll)
+        self.scroll.pack()
         self.numberLine = Text(
                 self.root, width=5, highlightbackground='#212835', bg='#212835',
-                borderwidth=0, highlightthickness=0, fg='white')
+                borderwidth=0, highlightthickness=0, fg='white',)
         self.numberLine.pack(side=LEFT, fill='both')
-        self.numberLine.config()
+        self.numberLine['yscrollcommand'] = self.scroll.set
+        self.numberLine.config(state=DISABLED)
 
         #text editor
         self.textEditor = Text(
                 self.root, tabs='34', highlightbackground='#212835', bg='#212835',
-                borderwidth=0, highlightthickness=0, insertbackground='white', fg='white')
+                borderwidth=0, highlightthickness=0, insertbackground='white', fg='white', wrap='none')
         self.textEditor.pack(fill='both', expand=True)
+        self.textEditor['yscrollcommand'] = self.scroll.set
 
         #root configuration
         self.root.config(menu=self.mainMenu,)
@@ -44,13 +56,36 @@ class mainWindow:
         self.root.bind("<Control-o>", self.openFile)
         self.root.bind("<Control-s>", self.saveFile)
         self.root.bind("<Control-n>", self.saveAsFile)
-        self.textEditor.bind("<Return>", self.initiatNumberLine)
-        #self.initiatNumberLine()
+        self.textEditor.bind("<BackSpace>", self.writeNumberLine)
+        self.textEditor.bind('<Button-5>', self._on_mousewheel_up)
+        self.textEditor.bind('<Button-4>', self._on_mousewheel_down)
+        self.textEditor.bind('<Up>', self._on_mousewheel_up)
+        self.textEditor.bind('<Down>', self._on_mousewheel_down)
+        self.numberLine.bind('<Button-5>', self._on_mousewheel_up)
+        self.numberLine.bind('<Button-4>', self._on_mousewheel_down)
+        self.numberLine.bind('<Up>', self._on_mousewheel_up)
+        self.numberLine.bind('<Down>', self._on_mousewheel_down)
+        self.textEditor.bind("<Return>", self.writeNumberLine)
+
+    #function for simultanous Scrollbar
+    def _simulScroll(self, *args, **kwargs):
+        eval('self.textEditor.yview(*args)')
+        eval('self.numberLine.yview(*args)')
+
+
+    def _on_mousewheel_up(self, event):
+        self.textEditor.yview_scroll(1, "units")
+        self.numberLine.yview_scroll(4, "units")
+
+    def _on_mousewheel_down(self, event):
+        self.textEditor.yview_scroll(-1, "units")
+        self.numberLine.yview_scroll(-4, "units")
 
 
     #function for opening files
     def _changeTitleName(self, *args, **kwargs):
         self.root.title(self.file.split('/')[-1])
+
 
     def openFile(self, *args, **kwargs):
         self.file = askopenfilename(initialdir = "c:/",
@@ -61,7 +96,7 @@ class mainWindow:
                 fileR = fileRead.read()
                 self.textEditor.insert(INSERT, fileR)
             self._changeTitleName()
-
+            self.writeNumberLine()
         except Exception as e:
             print(e)
 
@@ -91,12 +126,14 @@ class mainWindow:
         self.saveFile()
 
     #function for writing numbers on text widget
-    def initiatNumberLine(self, *args, **kwargs):
-        all = self.textEditor.get('0.0', END)
-        numlist = all.splitlines()
-        lineNumber = len(numlist)
-        print(numlist)
-        self.numberLine.insert(float(lineNumber), str(lineNumber)+'\n')
+
+    def writeNumberLine(self, *event):
+        displayLines = self.textEditor.count("1.0", "end", "lines")
+        self.numberLine.config(state=NORMAL)
+        self.numberLine.delete('1.0', END)
+        for item in range(1, displayLines+2):
+            self.numberLine.insert(float(item), str(item) + '\n')
+        self.numberLine.config(state=DISABLED)
 
 
 app = Tk()
